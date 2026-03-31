338f98f26fb67180b2717ba2c3524093bc213aae
Yarik174 <iovenko074@gmail.com>
2026-03-31 16:41:54 +0700

    auto: update file /Users/user/sales_engineer/magnum-opus/scripts/apollo_people_search.js

--- scripts/apollo_people_search.js
+++ scripts/apollo_people_search.js
@@ -226,7 +226,7 @@ async function searchPeopleAPI(page, params) {
 
       for (let pageNum = 1; pageNum <= maxPages; pageNum++) {
         const params = {
-          organization_domains: batchDomains,
+          q_organization_domains_list: batchDomains,
           person_titles: titles,
           person_seniorities: seniorities,
           page: pageNum,
