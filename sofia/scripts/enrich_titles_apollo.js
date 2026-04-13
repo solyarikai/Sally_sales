27424d514b759acd3560e26d7fb9282f10edcbb5
solyarikai <iovenko074@gmail.com>
2026-04-13 17:59:54 +0700

    auto: update file /Users/user/sales_engineer/sofia/scripts/enrich_titles_apollo.js

--- sofia/scripts/enrich_titles_apollo.js
+++ sofia/scripts/enrich_titles_apollo.js
@@ -111,6 +111,7 @@ async function searchDomains(page, domains, pageNum) {
     }
   }, {
     q_organization_domains_list: domains,
+    person_seniorities: ['c_suite', 'vp', 'director', 'manager', 'founder', 'owner', 'partner'],
     page: pageNum,
     per_page: 100,
     display_mode: 'explorer_mode',
