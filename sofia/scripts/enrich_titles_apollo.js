5c3bbc1a74bfa60bb6223b7472cacc5d2bdb237c
solyarikai <iovenko074@gmail.com>
2026-04-13 17:56:37 +0700

    auto: update file /Users/user/sales_engineer/sofia/scripts/enrich_titles_apollo.js

--- sofia/scripts/enrich_titles_apollo.js
+++ sofia/scripts/enrich_titles_apollo.js
@@ -10,7 +10,7 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
 const path = require('path');
-const csv = require('csv-parse/sync');
+
 
 puppeteer.use(StealthPlugin());
 
